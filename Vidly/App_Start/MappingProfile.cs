489714239667d60c173cd77a838c097b66928741
dima9917 <dima9917@gmail.com>
2018-08-04 11:24:10 -0700

    S6L68: AutoMapper

---
+++ Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //var config = new MapperConfiguration(cfg =>
+            //{
+            //    cfg.CreateMap<Customer, CustomerDto>();
+            //    cfg.CreateMap<CustomerDto, Customer>();
+            //});
+
+            this.CreateMap<Customer, CustomerDto>();
+            this.CreateMap<CustomerDto, Customer>().IgnoreAllPropertiesWithAnInaccessibleSetter(); //to ignore test prop
+
+        }
+    }
+}
